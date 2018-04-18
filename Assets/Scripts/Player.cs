0cb86987f7a58d39f8cfe4f7369731f678752c18
OWEN PENMAN <1736953@student.fife.ac.uk>
2018-04-18 15:14:19 +0100

    added starting basics
    added player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D myRigidBody;
+
+    [SerializeField]
+    private float MovementSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+           
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    { 
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+
+		
+	}
+    private void HandleMovement(float horizontal)
+    {
+        myRigidBody.velocity = new Vector2(horizontal*MovementSpeed, myRigidBody.velocity.y);
+
+    }
+
+}
